fe7e4f0152aebb2603f841a46fcda9110338daef
Emanuele <emacannizzo@gmail.com>
2024-01-19 22:06:35 +0100

    output_fun to device

--- utils.py
+++ utils.py
@@ -1288,6 +1288,8 @@ def train_approximator(dataset,
     model.to(device)
     if additional_variable:
         additional_variable = [var.to(device) for var in additional_variable]
+    output_fun.to(device)
+
 
     train_set_size = int(len(dataset) * 0.8)
     test_set_size = len(dataset) - train_set_size
